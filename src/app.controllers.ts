b52fb06ccd9f0e8035f664e3436329576ddc4380
Nicole <lancasternj@gmail.com>
2023-08-09 21:55:27 +0100

    completing whoami endpoint with JSON response header info

--- src/app.controllers.ts
+++ src/app.controllers.ts
@@ -1,4 +1,4 @@
-import Express from "express";
+import Express, { response } from "express";
 
 export const getBasicHtml = (
   request: Express.Request,
@@ -6,6 +6,20 @@ export const getBasicHtml = (
 ) => {
   try {
     return response.status(200).sendFile(`${__dirname}/views/index.html`);
+  } catch (err) {
+    return console.error(err);
+  }
+};
+
+export const getResponseHeader = (
+  request: Express.Request,
+  response: Express.Response,
+) => {
+  const ipAddress: string = request.ip;
+  const language = request.headers["accept-language"];
+  const software = request.headers["user-agent"];
+  try {
+    return response.status(200).send({ ipAddress, language, software });
   } catch (err) {
     return console.error(err);
   }
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
-import { getBasicHtml } from "./app.controllers";
+import { getBasicHtml, getResponseHeader } from "./app.controllers";
 export const app = express();
 
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static("public"));
 app.get("/", getBasicHtml);
+app.get("/api/whoami", getResponseHeader);
